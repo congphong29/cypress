6acb0a853848e5fa1c6bacb95c72deb19a13f13e
PHONG TRAN CONG <trancongphong@archway.co.jp>
2020-03-19 16:54:51 +0700

    add src

---
+++ cypress/integration/index.spec.js
@@ -0,0 +1,13 @@
+context('Integration test index page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('.get', () => {
+    cy.get('.v-card').should('have.class', 'v-card')
+    cy.get('.v-card__title').should('contain', 'Nuxt.js template')
+    cy.get('.v-card__text').find('a').should('have.length', 5)
+    cy.get('a').should('have.attr', 'href')
+      .debug()
+  })
+})
